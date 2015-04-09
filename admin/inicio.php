f55f239a57c1ce27a6f5a80040374813a785ce0c
McMachote <juanmielosado@hotmail.com>
2015-04-09 22:38:24 +0200

    admin/inicio
    Microdetalle pendiente, alinear (dentro del layout) bloque izquierdo con
    lista de usuarios

--- admin/inicio.php
+++ admin/inicio.php
@@ -1,14 +1,16 @@
 <?php ob_start(); ?>
-<div>
+
+<div class="col-11">
     <p>
         <img class="col-4" src="../img/line-graph.gif" class="img-responsive mini-logo">
         <img class="col-4" src="../img/line-graph.gif" class="img-responsive mini-logo">
         <img class="col-4" src="../img/line-graph.gif" class="img-responsive mini-logo">
     </p>
+    <div class="clear"></div>
 </div>
 <div>
-    <div class="col-4">
-        <div class="col-4">
+    <div class="col-3">
+        <div class="col-8">
             <ul class="no-style-list">
                 <li><a href="#"> Nº universidades</a></li>
                 <li><a href="#"> Nº carreras</a></li>
@@ -16,7 +18,7 @@
                 <li><a href="#"> Nº apuntes</a></li>
             </ul>
         </div>
-        <div class="col-1">
+        <div class="col-4">
             <ul class="no-style-list">
                 <li> 666</li>
                 <li> 22</li>
@@ -26,9 +28,60 @@
         </div>
     </div>
     <div class="col-8">
-
+        <div class="fila">
+            <p>
+                <span class="col-9">
+                    <span class="fa fa-file-text-o"></span>
+                    <strong><a href="#"> Rodolfo langostino</a></strong>
+                </span>
+                <span class="col-3"><span class="fa fa-file"></span> 13</span>
+            </p>
+            <div class="clear"></div>
+        </div>
+        <div class="fila">
+            <p>
+                <span class="col-9">
+                    <span class="fa fa-file-text-o"></span>
+                    <strong><a href="#"> Sinchan42</a></strong>
+                </span>
+                <span class="col-3"><span class="fa fa-file"></span> 7</span>
+            </p>
+            <div class="clear"></div>
+        </div>
+        <div class="fila">
+            <p>
+                <span class="col-9">
+                    <span class="fa fa-file-text-o"></span>
+                    <strong><a href="#"> Chiquito</a></strong>
+                </span>
+                <span class="col-3"><span class="fa fa-file"></span> 5</span>
+            </p>
+            <div class="clear"></div>
+        </div>
+        <div class="fila">
+            <p>
+                <span class="col-9">
+                    <span class="fa fa-file-text-o"></span>
+                    <strong><a href="#"> Matutano-man</a></strong>
+                </span>
+                <span class="col-3"><span class="fa fa-file"></span> 101</span>
+            </p>
+            <div class="clear"></div>
+        </div>
+        <div class="fila">
+            <p>
+                <span class="col-9">
+                    <span class="fa fa-file-text-o"></span>
+                    <strong><a href="#"> Uno_que_pasaba_por_aqui</a></strong>
+                </span>
+                <span class="col-3"><span class="fa fa-file"></span> -1</span>
+            </p>
+            <div class="clear"></div>
+        </div>
     </div>
+    <div class="clear"></div>    
 </div>
+
 <?php
 $contenido = ob_get_clean();
 require "../common/admin/layout.php";
