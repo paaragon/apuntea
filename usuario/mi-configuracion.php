51537f94891e8181d88e10c1a9f40ec24ee60c74
Kherdu <rafacaturla@gmail.com>
2015-04-14 17:38:41 +0200

    config actualizada

--- usuario/mi-configuracion.php
+++ usuario/mi-configuracion.php
@@ -1,42 +1,42 @@
 <?php ob_start(); ?>
 
-<div class="col-9" id="principal">
-    
-    <div>
-        <fieldset>
-            <legend>Datos personales: </legend>
-            Nombre completo: <Strong> Nombre y apellidos del usuario</Strong>   <input type="text" name="nombre" size="30"> <br>
-            E-mail:          <Strong> e-mail del usuario</Strong>               <input type="text" name="email" size="30"> <br>
-            Dirección:       <Strong>Calle falsa 123</Strong>                   <input type="text" name="direccion" size="60"> <br>
-            Universidad:     <Strong>Universidad en la que estudia</Strong>     <input type="text" name="universidad" size="30"> <br>
-            Imagen de perfil: <img src="../img/carnet.PNG"  id="foto-user">     <input type="image" value="Subir nueva imagen">  <br>
+<div id="principal">
+
+    <div class="col-5">
+        <form action="mi-configuracion.php" method="post">
+            <legend>Mis datos personales:</legend>
+            <span class="col-3"><label>Nombre completo:</label></span>
+            <span class="col-9"><input type="text" name="nombre" class="campo-formulario" placeholder="Introduzca su nombre de usuario" required=""></span>
+            <span class="col-3"><label>Email:</label></span>
+            <span class="col-9"><input type="email" name="nombre" class="campo-formulario" placeholder="Introduzca su nuevo e-mail" required=""></span>
+            <span class="col-3"><label>Dirección:</label></span>
+            <span class="col-9"><input type="text" name="nombre" class="campo-formulario" placeholder="Introduzca su dirección (opcional)"></span>
+            <span class="col-3"><label>Universidad:</label></span>
+            <span class="col-9"><input type="text" name="nombre" class="campo-formulario" placeholder="Introduzca su universidad" required=""></span>
             
-        </fieldset>
-        
-        
+            <input type="submit" value="Guardar datos personales" class="campo-formulario">
+        </form>
     </div>
-    <br>
-    <div>
-        <fieldset>
+    <div class="clear"></div>
+    <div class="col-5">
+        <form action="mi-configuracion.php" method="post">
             <legend>Privacidad: </legend>
             ¿Quién puede ver mi perfil? <br>
-            <input type="radio" name="visibilidad" value="Todo el mundo" checked="checked"/> Todo el mundo
-            <input type="radio" name="visibilidad" value="Solo amigos" checked="checked"/> Solo mis amigos
+            <label><input type="radio" name="vis-perfil" value="Todo el mundo" checked="checked"/> Todo el mundo</label>
+            <label><input type="radio" name="vis-perfil" value="Solo amigos" /> Solo mis amigos</label>
             <hr>
             ¿Quién puede ver mi actividad? <br>
-            <input type="radio" name="visibilidad" value="Todo el mundo" checked="checked"/> Todo el mundo
-            <input type="radio" name="visibilidad" value="Solo amigos" checked="checked"/> Solo mis amigos
+            <label><input type="radio" name="vis-actividad" value="Todo el mundo" checked="checked"/> Todo el mundo</label>
+            <label><input type="radio" name="vis-actividad" value="Solo amigos" /> Solo mis amigos</label>
             <hr>
-            ¿Quién puede ver mi perfil? <br>
-            <input type="radio" name="visibilidad" value="Todo el mundo" checked="checked"/> Todo el mundo
-            <input type="radio" name="visibilidad" value="Solo amigos" checked="checked"/> Solo mis amigos
-        </fieldset>
-        
+            ¿Quién puede encontrarme a través del buscador? <br>
+            <label><input type="radio" name="vis-buscador" value="Todo el mundo" checked="checked"/> Todo el mundo</label>
+            <label><input type="radio" name="vis-buscador" value="Solo amigos" /> Solo mis amigos</label><br><br>
+            <input type="submit" name="actualizar" value="Enviar" class="campo-formulario">
+        </form>
+
     </div>
-    <br>
-    <div>
-     <input type="submit" name="actualizar" value="Enviar">
-    </div>
+
 </div>
 
 <?php
