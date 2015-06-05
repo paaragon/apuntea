5d6c844a13a71ab1cd9abc92c29ef520ae53a193
Kherdu <rafacaturla@gmail.com>
2015-06-05 20:28:58 +0200

    revertir
    ignorar

--- admin/inicio.php
+++ admin/inicio.php
@@ -1,7 +1,8 @@
 <?php
-require __DIR__ . "/../controladores/ControladorAdmin.php";
+require "../controladores/ControladorAdmin.php";
 $controlador = new ControladorAdmin();
-$variables = $controlador->inicio();
+
+$variables = $controlador->inicioAdmin();
 
 ob_start();
 ?>
@@ -12,15 +13,15 @@ ob_start();
 <div class="row">
     <div class="col-md-4">
         <canvas id="myChart1"></canvas>
-        <h4 class="text-center"><strong>Nº usuarios registrados en los últimos 7 meses</strong></h4>
+        <h4 class="text-center"><strong>Gráfica 1</strong></h4>
     </div>
     <div class="col-md-4">
         <canvas id="myChart2"></canvas>
-        <h4 class="text-center"><strong>Universidades / apuntes (%)</strong></h4>
+        <h4 class="text-center"><strong>Gráfica 2</strong></h4>
     </div>
     <div class="col-md-4">
         <canvas id="myChart3"></canvas>
-        <h4 class="text-center"><strong>Nº apuntes en los últimos 7 meses</strong></h4>
+        <h4 class="text-center"><strong>Gráfica 3</strong></h4>
     </div>
 </div>
 <div class="col-3">
@@ -36,10 +37,10 @@ ob_start();
         </div>
         <div class="col-4">
             <ul class="no-style-list">
-                <li>666</li>
-                <li>22</li>
-                <li>101</li>
-                <li>9122</li>
+                <li><?php echo $variables['numuniversidades'] ?></li>
+                <li><?php echo $variables['numcarreras'] ?></li>
+                <li><?php echo $variables['numasignaturas'] ?></li>
+                <li><?php echo $variables['numapuntes'] ?></li>
             </ul>
         </div>
         <div class="clear"></div>
@@ -47,106 +48,47 @@ ob_start();
 </div>
 <div class="col-9">
     <h3>Última actividad:</h3>
-    <div class="fila">
-        <p>
-            <span class="col-10">
-                <span class="fa fa-users"></span>
-                <strong> <em>serfati</em> se ha añadido a tu lista de amigos</strong>
-            </span>
-            <span class="col-1"><span class="fa fa-thumbs-o-up"></span> 20</span>
-            <span class="col-1"><span class="fa fa-thumbs-o-down"></span> 2</span>
-        </p>
-        <div class="clear"></div>
-    </div>
-    <div class="fila">
-        <p>
-            <span class="col-10">
-                <span class="fa fa-pencil-square"></span>
-                <strong> <em>irepas01</em> ha modificado el archivo <em> Tema 1</em></strong>
-            </span>
-            <span class="col-1"><span class="fa fa-thumbs-o-up"></span> 20</span>
-            <span class="col-1"><span class="fa fa-thumbs-o-down"></span> 2</span>
-        </p>
-        <div class="clear"></div>
-    </div>
-    <div class="fila">
-        <p>
-            <span class="col-10">
-                <span class="fa fa-plus-square"></span>
-                <strong> <em>MrSlide22</em> ha añadido el archivo <em> Tema 3</em></strong>
-            </span>
-            <span class="col-1"><span class="fa fa-thumbs-o-up"></span> 20</span>
-            <span class="col-1"><span class="fa fa-thumbs-o-down"></span> 2</span>
-        </p>
-        <div class="clear"></div>
-    </div>
-    <div class="fila">
-        <p>
-            <span class="col-10">
-                <span class="fa fa-file-text-o"></span>
-                <strong> <em> Kherdu </em> forma parte del grupo <em> Apuntes Aplicaciones Web</em></strong>
-            </span>
-            <span class="col-1"><span class="fa fa-thumbs-o-up"></span> 20</span>
-            <span class="col-1"><span class="fa fa-thumbs-o-down"></span> 2</span>
-        </p>
-        <div class="clear"></div>
-    </div>
-    <div class="fila">
-        <p>
-            <span class="col-10">
-                <span class="fa fa-user-plus"></span>
-                <strong> <em> McMachote </em> te ha incluido en el grupo <em> Proyecto AW</em></strong>
-            </span>
-            <span class="col-1"><span class="fa fa-thumbs-o-up"></span> 20</span>
-            <span class="col-1"><span class="fa fa-thumbs-o-down"></span> 2</span>
-        </p>
-        <div class="clear"></div>
-    </div>
+    <?php foreach ($variables["grupos"] as $g): ?>
+        <div class="fila">
+            <p>
+                <span class="col-11">
+                    <a href="ver-grupo.php?id=<?php echo $g->id ?>"><span class="fa fa-circle-o-notch"></span> <?php echo $g["nombre"] ?> </a> ha sido creado
+                </span>
+                <span class="col-1"><span class="fa fa-users"></span><?php echo count($g->ownUsuariogrupoList) ?></span>
+            </p>
+            <div class="clear"></div>
+        </div>
+    <?php endforeach; ?>
+
+    <?php foreach ($variables["usuarios"] as $u): ?>
+        <div class="fila">
+            <p>
+                <span class="col-11">
+                    <a href="usuarios-detalles.php?id=<?php echo $u->id ?>"><span class="fa fa-users"></span> <?php echo $u["nick"] ?> </a> se ha registrado
+                </span>
+                <span class="col-1"><span class="fa fa-file"></span><?php echo count($u->ownApunteList) ?> </span>
+            </p>
+            <div class="clear"></div>
+        </div>
+    <?php endforeach; ?>
+
+    <?php foreach ($variables["apuntes"] as $a): ?>
+        <div class="fila">
+            <p>
+                <span class="col-9">
+                    <a href="ver-apunte.php?id=<?php echo $a->id ?>"><span class="fa fa-file"></span> <?php echo $a["titulo"] ?> </a> se han añadido
+                </span>
+                <span class="col-1"><span class="fa fa-thumbs-up"></span><?php echo $a->likes ?></span>
+                <span class="col-1"><span class="fa fa-thumbs-down"></span><?php echo $a->dislikes ?></span>
+                <span class="col-1"><span class="fa fa-eye"></span><?php echo $a->visualizaciones ?></span>
+            </p>
+            <div class="clear"></div>
+        </div>
+    <?php endforeach; ?>
 </div>
 <div class="clear"></div>
 <script>
     $(document).on("ready", function () {
-
-<?php
-$chart1 = array_reverse($variables["chart1"]);
-
-$primer_mes = key($chart1);
-$primer_valor = array_shift($chart1);
-
-$etiquetas1 = '"' . $primer_mes . '"';
-$valores1 = $primer_valor;
-
-foreach ($chart1 as $month => $value) {
-
-    $etiquetas1 .= ', "' . $month . '"';
-    $valores1 .=', ' . $value;
-}
-
-$chart2 = $variables["chart2"];
-
-$elem = array_shift($chart2);
-
-$i = 1;
-$data2 = '{value: ' . $elem["num"] . ', color: "rgba(70, 181, 82, ' . $i . ')", highlight: "rgba(70, 181, 82, ' . ($i - 0.1) . ')", label: "' . $elem["siglas"] . '" }';
-foreach ($chart2 as $elem) {
-    $i -= 0.2;
-    $data2 .= ',{value: ' . $elem["num"] . ', color: "rgba(70, 181, 82, ' . $i . ')", highlight: "rgba(70, 181, 82, ' . ($i - 0.1) . ')", label: "' . $elem["siglas"] . '" }';
-}
-
-$chart3 = array_reverse($variables["chart3"]);
-
-$primer_mes = key($chart3);
-$primer_valor = array_shift($chart3);
-
-$etiquetas3 = '"' . $primer_mes . '"';
-$valores3 = $primer_valor;
-
-foreach ($chart3 as $month => $value) {
-
-    $etiquetas3 .= ', "' . $month . '"';
-    $valores3 .=', ' . $value;
-}
-?>
 
         var canvas1 = document.getElementById("myChart1");
         var canvas2 = document.getElementById("myChart2");
@@ -161,46 +103,59 @@ foreach ($chart3 as $month => $value) {
 
         //Gráfica 1----------------------------------------------------
         var data1 = {
-            labels: [<?php echo $etiquetas1 ?>],
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
             datasets: [
                 {
+                    label: "My First dataset",
                     fillColor: "rgba(70, 181, 82, 0.2)",
                     strokeColor: "rgba(59, 152, 68, 0.5)",
                     pointColor: "rgba(59, 152, 68, 0.6)",
                     pointStrokeColor: "rgba(59, 152, 68, 0.8)",
                     pointHighlightFill: "#fff",
                     pointHighlightStroke: "rgba(220,220,220,1)",
-                    data: [<?php echo $valores1 ?>]
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                }
+            ]
+        };
+        var ctx = document.getElementById("myChart1").getContext("2d");
+        var myLineChart1 = new Chart(ctx).Line(data1);
+
+        //Gráfica 2----------------------------------------------------
+        var data2 = {
+            labels: ["January", "February", "March", "April", "May", "June"],
+            datasets: [
+                {
+                    label: "My First dataset",
+                    fillColor: "rgba(70, 181, 82, 0.2)",
+                    strokeColor: "rgba(59, 152, 68, 0.5)",
+                    highlightFill: "rgba(220,220,220,0.75)",
+                    highlightStroke: "rgba(220,220,220,1)",
+                    data: [65, 59, 80, 81, 56, 55]
                 }
             ]
         };
 
-        var ctx = document.getElementById("myChart1").getContext("2d");
-        var myLineChart2 = new Chart(ctx).Line(data1);
-        //Gráfica 2----------------------------------------------------
-        var data2 = [<?php echo $data2 ?>]
-
         var ctx = document.getElementById("myChart2").getContext("2d");
-        var myLineChart1 = new Chart(ctx).Pie(data2);
+        var myLineChart1 = new Chart(ctx).Bar(data2);
 
         //Gráfica 3----------------------------------------------------
         var data3 = {
-            labels: [<?php echo $etiquetas3 ?>],
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
             datasets: [
                 {
+                    label: "My First dataset",
                     fillColor: "rgba(70, 181, 82, 0.2)",
                     strokeColor: "rgba(59, 152, 68, 0.5)",
                     pointColor: "rgba(59, 152, 68, 0.6)",
                     pointStrokeColor: "rgba(59, 152, 68, 0.8)",
                     pointHighlightFill: "#fff",
                     pointHighlightStroke: "rgba(220,220,220,1)",
-                    data: [<?php echo $valores3 ?>]
+                    data: [50, 200, 70, 100, 15, 250, 200, 220]
                 }
             ]
         };
-
         var ctx = document.getElementById("myChart3").getContext("2d");
-        var myLineChart3 = new Chart(ctx).Line(data3);
+        var myLineChart1 = new Chart(ctx).Line(data3);
     });
 </script>
 <?php
